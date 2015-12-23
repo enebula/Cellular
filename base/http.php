60bca309f3b8142236fd2680aef5fbd0cfe2a791
mark <weixuan.1987@hotmail.com>
2015-12-23 21:31:53 +0800

    建立http基础类

---
+++ base/http.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *
+ * Cellular base http
+ *
+ * @author mark weixuan.1987@hotmail.com
+ * @version 1.0 2015-12-22
+ *
+ */
+
+namespace base;
+
+class http
+{
+  public function get()
+  {
+      echo 'ok';
+  }
+
+  public function post($url, $value)
+  {
+    $header = array();
+    $header[] = 'Content-Type: text/xml; charset=GBK2312';
+    $header[] = 'Content-Length: '.strlen($value);
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    //curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //跳过SSL检查
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $value);
+    $output = curl_exec($curl);
+    curl_close($curl);
+    //if (!$output) return false;
+    return $output;
+  }
+}
+
+?>
---
+++ base/test.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ * Cellular Framework
+ *
+ * @author mark weixuan.1987@hotmail.com
+ * @version 1.0 2015-12-9
+ *
+ */
+namespace base;
+
+class test {
+
+
+	public function b()
+	{
+		echo '*bb*';
+	}
+
+}
+
+?>
