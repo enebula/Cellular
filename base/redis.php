886ab709762ab43b2b2bf61d5bd2def8066fb439
mark <weixuan.1987@hotmail.com>
2016-01-26 09:44:01 +0800

    增加服务器环境检查工具

---
+++ base/redis.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * Cellular Framework
+ * redis封装
+ *
+ * @author mark weixuan.1987@hotmail.com
+ * @version 1.0 2015-12-9
+ *
+ */
+
+namespace base;
+
+class redis {
+
+    private $r;
+    private $host;
+    private $port;
+    private $db;
+
+    public function __construct($config)
+    {
+        $this->host = '127.0.0.1';
+        $this->port = '6379';
+        $this->db = 0;
+        $r = new \Redis();
+    }
+
+    public function conent()
+    {
+        $r->pconnect($this->host, $this->port);
+    }
+
+}
+?>
---
+++ tools/check_service.php
@@ -0,0 +1,5 @@
+<?php
+    //检查mb_strtolower()
+    if (!function_exists('mb_strtolower')) die('Not install mbString');
+    die('ok');
+?>
