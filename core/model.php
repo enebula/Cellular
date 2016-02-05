5e7b80a7f640cc3c3f77823de9fe06255c9fa278
Cloud <66999882@qq.com>
2016-02-05 13:39:39 +0800

    Format Code

--- core/model.php
+++ core/model.php
@@ -19,59 +19,58 @@ class Model extends DB {
     /**
      * 根据主键取出一条数据
      */
-     protected function find() {
+    protected function find() {
 
-     }
+    }
 
     /**
      * 行数统计
      */
-     protected function count() {
+    protected function count() {
 
-     }
+    }
 
-     /**
-      * 最大值
-      */
-      protected function max() {
+    /**
+    * 最大值
+    */
+    protected function max() {
 
-      }
+    }
 
-      /**
-       * 最小值
-       */
-       protected function min() {
+    /**
+    * 最小值
+    */
+    protected function min() {
 
-       }
+    }
 
-       /**
-        * 平均值
-        */
-        protected function avg() {
+    /**
+    * 平均值
+    */
+    protected function avg() {
 
-        }
+    }
 
-        /**
-         * 累加值
-         */
-         protected function sum() {
+    /**
+    * 累加值
+    */
+    protected function sum() {
 
-         }
+    }
 
-         /**
-          * 字段自增
-          */
-          protected function increment() {
+    /**
+    * 字段自增
+    */
+    protected function increment() {
 
-          }
+    }
 
-          /**
-           * 字段自减
-           */
-           protected function decrement() {
+    /**
+    * 字段自减
+    */
+    protected function decrement() {
 
-           }
+    }
 
 }
-
 ?>
