9f91a9470242bf0db4038e05e44229ae41bee634
Cloud <66999882@qq.com>
2016-02-05 11:28:47 +0800

    Add Model Class

---
+++ core/model.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Cellular Framework
+ * 模型基类
+ * @copyright Cellular Team
+ */
+namespace core;
+use Cellular;
+
+class Model extends DB {
+
+    /**
+     * 取出所有记录
+     */
+    protected function all() {
+
+    }
+
+    /**
+     * 根据主键取出一条数据
+     */
+     protected function find() {
+
+     }
+
+    /**
+     * 行数统计
+     */
+     protected function count() {
+
+     }
+
+     /**
+      * 最大值
+      */
+      protected function max() {
+
+      }
+
+      /**
+       * 最小值
+       */
+       protected function min() {
+
+       }
+
+       /**
+        * 平均值
+        */
+        protected function avg() {
+
+        }
+
+        /**
+         * 累加值
+         */
+         protected function sum() {
+
+         }
+
+         /**
+          * 字段自增
+          */
+          protected function increment() {
+
+          }
+
+          /**
+           * 字段自减
+           */
+           protected function decrement() {
+
+           }
+
+}
+
+?>
