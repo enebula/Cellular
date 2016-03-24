2b4fa2d31750150ecb83acc70727be4e35c5d35f
mark <weixuan.1987@hotmail.com>
2016-03-25 00:34:04 +0800

    update

--- core/page.php
+++ core/page.php
@@ -6,24 +6,21 @@
  */
 
 namespace core;
-
+use Cellular;
 
 class Page
 {
-    function display($total, $page, $limit, $param = null)
+    public function display($total, $page, $limit, $param = null)
     {
         $result = array();
         if ($total < 1) return $result;
         if (!$page) $page = 1;
-        $result['current'] = $page; //当前页
-        $result['total'] = $total; //总记录数
-        $result['limit'] = $limit; //显示记录数
         $result['total_page'] = ceil($total / $limit); //分页总数
         if ($result['total_page'] > 1) {
-            $result['previous'] = $this->redirect($page - 1, $param); //上页链接
-            $result['next'] = $this->redirect($page + 1, $param); //下页链接
-            $result['first'] = $this->redirect(1, $param); //首页链接
-            $result['last'] = $this->redirect($result['total_page'], $param); //尾页链接
+            $result['previous'] = $this->url($page > 1 ? $page - 1 : 1, $param); //上页链接
+            $result['next'] = $this->url($page < $result['total_page'] ? $page + 1 : $result['total_page'], $param); //下页链接
+            $result['first'] = $this->url(1, $param); //首页链接
+            $result['last'] = $this->url($result['total_page'], $param); //尾页链接
         }
         //计算按步长分页
         $result['step'] = array();
@@ -42,18 +39,16 @@ class Page
         }
         $temp = array();
         for ($i = $start; $i <= $end; $i++) $temp[] = intval($i);
-        foreach ($temp as $key => $value) $result['step'][$value] = $this->redirect($value, $param);
+        foreach ($temp as $key => $value) $result['step'][$value] = $this->url($value, $param);
         return $result;
     }
 
-    private function redirect($page, $param = null)
+    private function url($page, $param)
     {
+        $controller = isset($param['controller']) ? $param['controller'] : null;
+        $action = isset($param['action']) ? $param['action'] : null;
         $param['page'] = $page;
-        $url = null;
-        foreach ($param as $key => $val) {
-            $url .= '&' . $key . '=' . $val;
-        }
-        return '?' . substr($url, 1);
+        return Cellular::getURL($controller, $action, $param);
     }
 }
 ?>
