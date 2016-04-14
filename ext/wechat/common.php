9954fa488282bbee2c581528388169f3e8778f67
Cloud <66999882@qq.com>
2016-04-14 18:28:10 +0800

    update

--- ext/wechat/common.php
+++ ext/wechat/common.php
@@ -194,31 +194,6 @@ class Common
     }
 
     /**
-     * curl 获取数据
-     * @param $url
-     * @param int $timeOut
-     * @return bool|string
-     */
-    public static function curl($url, $timeOut = 30)
-    {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_TIMEOUT, $timeOut);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        $r = curl_exec($ch);
-        if (curl_errno($ch) !== 0) {
-            return false;
-        }
-        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        if ($status != 200) {
-            return false;
-        }
-        curl_close($ch);
-        return $r;
-    }
-
-    /**
      * 随机字符串
      * @return null|string
      */
@@ -305,20 +280,47 @@ class Common
     }
 
     /**
+     * curl 获取数据
+     * @param $url
+     * @param int $second
+     * @return bool|string
+     */
+    public static function curl($url, $second = 30)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $second);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $r = curl_exec($ch);
+        if (curl_errno($ch) !== 0) {
+            return false;
+        }
+        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if ($status != 200) {
+            return false;
+        }
+        curl_close($ch);
+        return $r;
+    }
+
+    /**
      * 以post方式提交xml到对应的接口url
      *
      * @param string $xml 需要post的xml数据
      * @param string $url url
-     * @param bool $useCert 是否需要证书，默认不需要
+     * @param bool $cert 是否需要证书，默认不需要
      * @param int $second url执行超时时间，默认30s
      * @return mixed
      * @throws WxPayException
      */
-    public static function postXmlCurl($xml, $url, $useCert = false, $second = 30)
+    public static function postXmlCurl($xml, $url, $cert = false, $second = 30)
     {
         $ch = curl_init();
-        //设置超时
-        curl_setopt($ch, CURLOPT_TIMEOUT, $second);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $second); # 设置超时
+        curl_setopt($ch, CURLOPT_HEADER, FALSE); # 设置 header
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); # 要求结果为字符串且输出到屏幕上
 
         /***
          * //如果有配置代理这里就设置代理
@@ -330,35 +332,30 @@ class Common
          * }
          ***/
 
-        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);//严格校验
-        //设置header
-        curl_setopt($ch, CURLOPT_HEADER, FALSE);
-        //要求结果为字符串且输出到屏幕上
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2); # 严格校验
 
-        if ($useCert == true) {
-            //设置证书
-            //使用证书：cert 与 key 分别属于两个.pem文件
+        if ($cert == true) {
+            # 设置证书
+            # 使用证书  cert 与 key 分别属于两个 pem 文件
             curl_setopt($ch, CURLOPT_SSLCERTTYPE, 'PEM');
             curl_setopt($ch, CURLOPT_SSLCERT, realpath('./config/cert') . DIRECTORY_SEPARATOR . 'apiclient_cert.pem');
             curl_setopt($ch, CURLOPT_SSLKEYTYPE, 'PEM');
             curl_setopt($ch, CURLOPT_SSLKEY, realpath('./config/cert') . DIRECTORY_SEPARATOR . 'apiclient_key.pem');
         }
-        //post提交方式
+        # post提交方式
         curl_setopt($ch, CURLOPT_POST, TRUE);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-        //运行curl
+        # 运行curl
         $data = curl_exec($ch);
-        //返回结果
+        # 返回结果
         if ($data) {
             curl_close($ch);
             return $data;
         } else {
             $error = curl_errno($ch);
             curl_close($ch);
-            die("curl出错，错误码:$error");
+            die('curl 出错 错误码:' . $error);
         }
     }
 }
