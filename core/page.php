c0054e151cf86f9582137a6d86e88d54697fccba
mark <weixuan.1987@hotmail.com>
2016-04-20 14:57:21 +0800

    update

--- core/page.php
+++ core/page.php
@@ -10,17 +10,17 @@ use Cellular;
 
 class Page
 {
-    public function display($total, $page, $limit, $param = null)
+    public static function display($total, $page, $limit, $param = null)
     {
         $result = array();
         if ($total < 1) return $result;
         if (!$page) $page = 1;
         $result['total_page'] = ceil($total / $limit); //分页总数
         if ($result['total_page'] > 1) {
-            $result['previous'] = $this->url($page > 1 ? $page - 1 : 1, $param); //上页链接
-            $result['next'] = $this->url($page < $result['total_page'] ? $page + 1 : $result['total_page'], $param); //下页链接
-            $result['first'] = $this->url(1, $param); //首页链接
-            $result['last'] = $this->url($result['total_page'], $param); //尾页链接
+            $result['previous'] = self::url($page > 1 ? $page - 1 : 1, $param); //上页链接
+            $result['next'] = self::url($page < $result['total_page'] ? $page + 1 : $result['total_page'], $param); //下页链接
+            $result['first'] = self::url(1, $param); //首页链接
+            $result['last'] = self::url($result['total_page'], $param); //尾页链接
         }
         //计算按步长分页
         $result['step'] = array();
@@ -39,11 +39,11 @@ class Page
         }
         $temp = array();
         for ($i = $start; $i <= $end; $i++) $temp[] = intval($i);
-        foreach ($temp as $key => $value) $result['step'][$value] = $this->url($value, $param);
+        foreach ($temp as $key => $value) $result['step'][$value] = self::url($value, $param);
         return $result;
     }
 
-    private function url($page, $param)
+    private static function url($page, $param)
     {
         $controller = isset($param['controller']) ? $param['controller'] : null;
         $action = isset($param['action']) ? $param['action'] : null;
